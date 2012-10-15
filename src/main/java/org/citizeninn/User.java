41b63c28f4664ffdc8671736c2bf63bc4ee3f864
Boris Zhurkevich <borys86@gmail.com>
2012-10-15 21:42:22 +0200

    Added User class. Bacis class for user entity.

---
+++ src/main/java/org/citizeninn/User.java
@@ -0,0 +1,45 @@
+package org.citizeninn;
+
+import java.util.Locale.Category;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "USERS")
+public class User {
+	
+	private String firstName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	private String lastName;
+	
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+		
+	private Long id;
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
