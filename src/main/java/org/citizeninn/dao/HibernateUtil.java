12ee88681f8db7d6fd5431c04affaaf357438172
Boris Tarasov <boris@callmeboris.com>
2013-08-05 20:42:39 +0100

    fixed HibernateUtil.java

--- src/main/java/org/citizeninn/dao/HibernateUtil.java
+++ src/main/java/org/citizeninn/dao/HibernateUtil.java
@@ -1,41 +1,30 @@
 package org.citizeninn.dao;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-	static SessionFactory sessionFactory = null;
+	private static SessionFactory sessionFactory;
 
-	public SessionFactory getSessionFactory() {
+	static {
+		try {
+			sessionFactory = new Configuration().configure()
+					.buildSessionFactory();
+
+		} catch (Exception e) {
+			throw new ExceptionInInitializerError();
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+
 		return sessionFactory;
 	}
 
-	static Session session = null;
-
-	@SuppressWarnings("deprecation")
-	protected static Session setUp() throws Exception {
-
-
-
-		try {
-
-			// A SessionFactory is set up once for an application
-			sessionFactory = new Configuration().configure() // configures
-																// settings
-																// from
-																// hibernate.cfg.xml
-					.buildSessionFactory();
-			session = sessionFactory.openSession();
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return session;
-
+	public static void shutdown() {
+		getSessionFactory().close();
 	}
+
 
 }
